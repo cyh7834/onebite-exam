4e8449a6e5d07dd0501a05d2d0b87a336d73bb38
cyh7834 <cyh7834@gmail.com>
2026-01-26 20:23:26 +0900

    Zustand 미들웨어 1

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,45 +1,41 @@
-import {create} from "zustand";
+import { create } from "zustand";
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-type Store = {
-    count: number;
-    actions: {
-     increase: () => void;
-     decrease: () => void;   
-    }
-};
-
-export const useCountStore = create<Store>((set, get)=> ({
-        count: 0,
-        actions: {
-            increase: () => {
-            set((store) => ({
-                    count: store.count + 1
-                }
-            ))
+// state 타입이 자동으로 추론
+export const useCountStore = create(
+  immer( // 상태 업데이트를 자동으로 불변하게 관리해줌
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        increase: () => {
+          set((state) => {
+            state.count += 1;
+          });
         },
         decrease: () => {
-            set((store) => ({
-                    count: store.count - 1
-                }
-            ))
-        }
-        }
-    }));
+          set((state) => {
+            state.count -= 1;
+          });
+        },
+      },
+    })),
+  ),
+);
 
 export const useCount = () => {
-    const count = useCountStore((store) => store.count);
+  const count = useCountStore((store) => store.count);
 
-    return count;
-}
+  return count;
+};
 
 export const useIncreaseCount = () => {
-    const increase = useCountStore((store) => store.actions.increase);
+  const increase = useCountStore((store) => store.actions.increase);
 
-    return increase;
-}
+  return increase;
+};
 
 export const useDecreaseCount = () => {
-    const decrease = useCountStore((store) => store.actions.decrease);
+  const decrease = useCountStore((store) => store.actions.decrease);
 
-    return decrease;
-}
+  return decrease;
+};
