23d4dbcb1526a8f3623198386df0bbda4bf6e11c
cyh7834 <cyh7834@gmail.com>
2026-01-28 20:17:04 +0900

    투두리스트 UI 구현하기

---
+++ src/pages/todo-list-page.tsx
@@ -0,0 +1,28 @@
+import TodoEditor from "@/components/todo-list/todo-editor";
+import TodoItem from "@/components/todo-list/todo-item";
+
+const dummyTodos = [
+  {
+    id: 1,
+    content: "Todo 1",
+  },
+  {
+    id: 2,
+    content: "Todo 2",
+  },
+  {
+    id: 3,
+    content: "Todo 3",
+  },
+];
+export default function TodoListPage() {
+  return (
+    <div className="flext flex-col gap-5 p-5">
+      <h1 className="text-2xl font-bold">TodoList</h1>
+      <TodoEditor></TodoEditor>
+      {dummyTodos.map((todo) => (
+        <TodoItem key={todo.id} {...todo}></TodoItem>
+      ))}
+    </div>
+  );
+}
