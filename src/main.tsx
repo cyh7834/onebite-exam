11c4ed08e6d39be094f60458b688c4fa7dfe4c4c
cyh7834 <cyh7834@gmail.com>
2026-02-01 21:29:42 +0900

    데이터 조회 요청 관리하기

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+// 모든 컴포넌트에서 쿼리 클라이언트 라는 스토어에 API 요청의 상태를 보관할 수 있음
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
