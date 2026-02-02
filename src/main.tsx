f4cc2e4a920c1a37e367cdd526217f5f7fbcb889
cyh7834 <cyh7834@gmail.com>
2026-02-02 20:26:49 +0900

    tanstack react query devtools 설치

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // 모든 컴포넌트에서 쿼리 클라이언트 라는 스토어에 API 요청의 상태를 보관할 수 있음
 const queryClient = new QueryClient();
@@ -10,6 +11,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools></ReactQueryDevtools>
       <App />
     </QueryClientProvider>
   </BrowserRouter>,
