8ccfcbca389bf5d449c7564c840dc91fc5720648
cyh7834 <cyh7834@gmail.com>
2026-02-05 21:06:47 +0900

    캐싱 메커니즘 이해하기 2

--- src/hooks/queries/use-todo-data-by-id.ts
+++ src/hooks/queries/use-todo-data-by-id.ts
@@ -5,6 +5,7 @@ export function useTodoDataById (id:number) {
     return useQuery({
     queryFn:() => fetchTodoById(id),
     queryKey: ["todos", id],
-    staleTime: 5000
+    staleTime: 5000, // Fresh 상황에서만 의미있음
+    gcTime: 5000 // Inactive 상황에서만 의미있음
   });
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // 모든 컴포넌트에서 쿼리 클라이언트 라는 스토어에 API 요청의 상태를 보관할 수 있음
-const queryClient = new QueryClient();
+const queryClient = new QueryClient(); // defaultOptions, queries 옵션을 통해 전역 설정으로도 가능
+// 보통 staleTime은 0, gcTime은 5분 정도로 설정.
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
