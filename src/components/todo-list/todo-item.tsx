e17857ebfab58abe6bef01c4ce5190ba3b101ff4
cyh7834 <cyh7834@gmail.com>
2026-02-12 21:44:51 +0900

    캐시 데이터 다루기 3 - 낙관적 업데이트 (1)

--- src/components/todo-list/todo-item.tsx
+++ src/components/todo-list/todo-item.tsx
@@ -1,22 +1,28 @@
-import { useDeleteTodo } from "@/store/todos";
 import { Button } from "../ui/button";
 import { Link } from "react-router";
+import type { Todo } from "@/types";
+import { useUpdateTodoMutation } from "@/hooks/mutations/use_update-todo-mutation";
 
-export default function TodoItem({
-  id,
-  content,
-}: {
-  id: number;
-  content: string;
-}) {
-  const deleteTodo = useDeleteTodo();
-  const handleDeleteClick = () => {
-    deleteTodo(id);
+export default function TodoItem({ id, content, isDone }: Todo) {
+  const { mutate } = useUpdateTodoMutation();
+  const handleDeleteClick = () => {};
+  const handleCheckbokClick = () => {
+    mutate({
+      id,
+      isDone: !isDone,
+    });
   };
 
   return (
     <div className="flex items-center justify-between border p-2">
-      <Link to={`/todolist/${id}`}>{content}</Link>
+      <div className="flex gap-5">
+        <input
+          type={"checkbox"}
+          checked={isDone}
+          onClick={handleCheckbokClick}
+        ></input>
+        <Link to={`/todolist/${id}`}>{content}</Link>
+      </div>
       <Button onClick={handleDeleteClick} variant={"destructive"}>
         삭제
       </Button>
