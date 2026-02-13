f4367cadf3cd2bd3caeed96f8b2b3995d875b0bc
cyh7834 <cyh7834@gmail.com>
2026-02-13 22:01:08 +0900

    캐시 데이터 다루기 4 - 낙관적 업데이트 (2)

--- src/hooks/mutations/use_update-todo-mutation.ts
+++ src/hooks/mutations/use_update-todo-mutation.ts
@@ -8,7 +8,12 @@ export function useUpdateTodoMutation() {
 
     return useMutation({
         mutationFn: updateTodo,
-        onMutate:(updatedTodo) => {
+        onMutate: async (updatedTodo) => {
+            await queryClient.cancelQueries({
+                queryKey: QUERY_KEYS.todo.list
+            });
+
+            const prevTodos = queryClient.getQueryData<Todo[]>(QUERY_KEYS.todo.list);
             queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list,
                 (prevTodos) => {
                     if (!prevTodos)
@@ -18,6 +23,23 @@ export function useUpdateTodoMutation() {
                     
                 }
             )
+
+            return {
+                prevTodos
+            };
+        },
+        onError: (error, variable, context) => {
+            if (context && context.prevTodos) {
+                queryClient.setQueryData<Todo[]> (
+                    QUERY_KEYS.todo.list,
+                    context.prevTodos,
+                )
+            }
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries({
+                queryKey: QUERY_KEYS.todo.list
+            })
         }
     });
 }
