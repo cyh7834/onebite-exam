9295e64bd37fd4a9a17a99d9c6655267d13e3e9e
cyh7834 <cyh7834@gmail.com>
2026-02-15 22:21:00 +0900

    투두 삭제 기능 만들기

--- src/components/todo-list/todo-item.tsx
+++ src/components/todo-list/todo-item.tsx
@@ -1,13 +1,18 @@
 import { Button } from "../ui/button";
 import { Link } from "react-router";
 import type { Todo } from "@/types";
-import { useUpdateTodoMutation } from "@/hooks/mutations/use_update-todo-mutation";
+import { useUpdateTodoMutation } from "@/hooks/mutations/use-update-todo-mutation";
+import { useDeleteTodoMutation } from "@/hooks/mutations/use-delete-todo-mutation";
 
 export default function TodoItem({ id, content, isDone }: Todo) {
-  const { mutate } = useUpdateTodoMutation();
-  const handleDeleteClick = () => {};
+  const { mutate: updateTodo } = useUpdateTodoMutation();
+  const { mutate: deleteTodo, isPending: isDeleteTodoPending } =
+    useDeleteTodoMutation();
+  const handleDeleteClick = () => {
+    deleteTodo(id);
+  };
   const handleCheckbokClick = () => {
-    mutate({
+    updateTodo({
       id,
       isDone: !isDone,
     });
@@ -17,13 +22,18 @@ export default function TodoItem({ id, content, isDone }: Todo) {
     <div className="flex items-center justify-between border p-2">
       <div className="flex gap-5">
         <input
+          disabled={isDeleteTodoPending}
           type={"checkbox"}
           checked={isDone}
           onClick={handleCheckbokClick}
         ></input>
         <Link to={`/todolist/${id}`}>{content}</Link>
       </div>
-      <Button onClick={handleDeleteClick} variant={"destructive"}>
+      <Button
+        disabled={isDeleteTodoPending}
+        onClick={handleDeleteClick}
+        variant={"destructive"}
+      >
         삭제
       </Button>
     </div>
